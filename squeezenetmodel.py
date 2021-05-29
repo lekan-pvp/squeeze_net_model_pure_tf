5101a9c3ea8d246436e9972c072bddd00932151a
Vladislav Popov <lekan.lekan@gmail.com>
2021-05-29 23:34:09 +0300

    data augmentation

--- squeezenetmodel.py
+++ squeezenetmodel.py
@@ -6,4 +6,24 @@ class SqueezeNetModel(object):
         self.original_dim = original_dim
         self.resize_dim = resize_dim
         self.output_size = output_size
+
+    # Random crop and flip images
+    def random_crop_and_flip(self, float_image):
+        crop_image = tf.random_crop(float_image, size=[self.resize_dim, self.resize_dim, 3])
+        updated_image = tf.image.random_flip_left_right(crop_image)
+        return updated_image
+
+    # Data augmentation
+    def image_preprocessing(self, data, is_training):
+        reshaped_image = tf.reshape(data, [3, self.original_dim, self.original_dim])
+        transposed_image = tf.transpose(reshaped_image, [1, 2, 0])
+        float_image = tf.cast(transposed_image, tf.float32)
+        if is_training:
+            updated_image = self.random_crop_and_flip(float_image)
+        else:
+            updated_image = tf.image.resize_with_crop_or_pad(float_image, self.resize_dim, self.resize_dim)
+        standartized_image = tf.image.per_image_standardization(updated_image)
+        return standartized_image
+        
+
         
